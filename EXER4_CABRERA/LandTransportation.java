643b9899c8c8f092c533a335b86a8419f8963f3e
Von Philippe T. Cabrera <vonphilippe@gmail.com>
2026-01-04 16:44:40 +0800

    Delete EXER4_CABRERA directory

--- EXER4_CABRERA/LandTransportation.java
+++
@@ -1,8 +0,0 @@
-public class LandTransportation extends Transportation {
-    private int wheels;
-
-    public LandTransportation(String name, int capacity, int wheels){
-        super(name, capacity, 0.0);
-        this.wheels = wheels;
-    }
-}
--- EXER4_CABRERA/Ship.java
+++
@@ -1,9 +0,0 @@
-public class Ship extends WaterTransportation{
-    public Ship(int capacity, double displacementTons){
-        super("Ship", capacity, displacementTons);
-    }
-
-    public void calculateSpeed(){
-        speed = 25.0;
-    }
-}
